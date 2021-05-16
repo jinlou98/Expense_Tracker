62da94b47afbea0bb70d4245c1e71bf188e0a8b4
Jin Lou <jinlou@bu.edu>
2021-05-16 15:21:24 -0400

    Split four expenses items into one expenses component.

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -7,9 +7,9 @@ function ExpenseDate(props) {
 
     return (
     <div className="expense-date">        
-        <div className="expense-month">{month}</div>
-        <div className="expense-year">{year}</div>
-        <div className="expense-date">{day}</div>
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__year">{year}</div>
+        <div className="expense-date__day">{day}</div>
     </div>
     )
 }
