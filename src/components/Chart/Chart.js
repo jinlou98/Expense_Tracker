905f51060a090048bc7b90d78a2d6c6be8f65c75
Jin Lou <jinlou@bu.edu>
2021-05-18 12:48:37 -0400

    Fix chart bar.

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import ChartBar from "./ChartBar";
-import "./Chart.css";
+import ChartBar from './ChartBar';
+import './Chart.css';
 
 const Chart = (props) => {
-    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
-    const totalMaximum = Math.max(...dataPointValues);
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
 
   return (
-    <div className="Chart">
-      {props.dataPoints.map((dataPoints) => (
+    <div className='chart'>
+      {props.dataPoints.map((dataPoint) => (
         <ChartBar
-          key={dataPoints.label}
-          value={dataPoints.value}
+          key={dataPoint.label}
+          value={dataPoint.value}
           maxValue={totalMaximum}
-          label={dataPoints.label}
-        ></ChartBar>
+          label={dataPoint.label}
+        />
       ))}
     </div>
   );
